d2fbd6604255c781bf48df1d21c133fb4490fbb9
Alisa Esh <AEsh@eshcom.com>
2021-06-09 15:54:59 -0400

    reproduce

---
+++ Xam5TabView/Xam5TabView/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.CommunityToolkit.UI.Views;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xam5TabView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            GenerateUI();
+            OpenToSecondTab();
+        }
+
+        void GenerateUI()
+        {
+            var firstTabViewItem = new TabViewItem()
+            {
+                Text = "index 0",
+                Content = new Label() { Text = "Tab index 0 content" },
+                Padding = new Thickness(15, 0)
+            };
+
+            uiTabView.TabItems.Add(firstTabViewItem);
+
+            var secondTabViewItem = new TabViewItem()
+            {
+                Text = "index 1",
+                Content = new Label() { Text = "Tab index 1 content" },
+                Padding = new Thickness(15, 0)
+            };
+
+            uiTabView.TabItems.Add(secondTabViewItem);
+
+            var thirdTabViewItem = new TabViewItem()
+            {
+                Text = "index 2",
+                Content = new Label() { Text = "Tab index 2 content" },
+                Padding = new Thickness(15, 0)
+            };
+
+            uiTabView.TabItems.Add(thirdTabViewItem);
+        }
+
+        void OpenToSecondTab() => uiTabView.SelectedIndex = 1;
+    }
+}
+
